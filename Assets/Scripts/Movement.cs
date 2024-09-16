ac52281e2e8e0d79f300a99091083d98a8a24e1e
Halil İbrahim Özdemir <hbrahim.ozdemir@gmail.com>
2024-09-16 16:38:59 +0300

    Main files of JetBooster Game
    Its a clone game to understand unity basics

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float mainThrust=1000f;
+    [SerializeField] float rotationThrust = 100f;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb =GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+
+        }
+    }
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);// (0,0,1)
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    public void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; // freezing rotation so we can manually rotate.
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // un freezing rotation so the physics system can takeover.
+    }
+}
